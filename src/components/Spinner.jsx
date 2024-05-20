ee9feed0826b03a5edd3fc0da2473667f0b9aba5
Vishal Mourya <117056895+Vishal-200803@users.noreply.github.com>
2024-05-20 09:18:01 +0530

    Add files via upload

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./spinner.css"
+const Spinner = () => {
+  return (
+    <div className='spinner'>sd</div>
+  )
+}
+
+export default Spinner
---
+++ src/redux/store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { CardSlice } from "./slices/CartSlice";
+
+
+export const store = configureStore({
+    reducer:{
+        cart: CardSlice.reducer
+    }
+})
